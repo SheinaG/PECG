7db754b2a69d5c5a1f44a1fafd66ac0b8b32dd7e
SheinaG <sheina@campus.tecnion.ac.il>
2021-08-23 14:08:52 +0300

    first commit2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name= "pebm",
-    version="0.0.10",
+    version="0.0.11",
     use_scm_version={
         "root": '..',
         "relative_to": __file__,
