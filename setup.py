7f793781fd510e17c3e6572f4557218f888f15e7
SheinaG <sheina@campus.tecnion.ac.il>
2021-08-23 13:55:26 +0300

    first commit1

--- setup.py
+++ setup.py
@@ -28,13 +28,13 @@ setuptools.setup(
     
     package_dir={"": "pebm"},
     packages={"ebm",
-              'c_files',
-              'wavedet_exe'},
+              'ebm/c_files',
+              'ebm/wavedet_exe'},
             
     package_data={
         'ebm' : ['*'],
-        'c_files': ['*'],
-        'wavedet_exe': ['*']
+        'ebm/c_files': ['*'],
+        'ebm/wavedet_exe': ['*']
     },
     include_package_data=True,
     python_requires=">=3.6",
