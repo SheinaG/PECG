b0356f48e0e01142ae20ee28b624bb53f9d4d8cf
SheinaG <sheina@campus.tecnion.ac.il>
2021-08-28 20:46:14 +0300

    empty array

--- pebm/ebm/Statistics.py
+++ pebm/ebm/Statistics.py
@@ -10,13 +10,14 @@ def statistics(data_dict):
         data = data[data != -1]
         if not data:
             stat_dict[i] = {'mean': -1, 'median': -1, 'min': -1, 'max': -1, 'iqr': -1, 'std': -1}
-        mean_ = np.mean(data)
-        median_ = np.median(data)
-        min_ = np.amin(data)
-        max_ = np.amax(data)
-        q75, q25 = np.percentile(data, [75, 25])
-        iqr_ = q75 - q25
-        std_ = np.std(data)
-        stat_dict[i] = {'mean': mean_, 'median': median_, 'min': min_, 'max': max_, 'iqr': iqr_, 'std': std_}
+        else:
+            mean_ = np.mean(data)
+            median_ = np.median(data)
+            min_ = np.amin(data)
+            max_ = np.amax(data)
+            q75, q25 = np.percentile(data, [75, 25])
+            iqr_ = q75 - q25
+            std_ = np.std(data)
+            stat_dict[i] = {'mean': mean_, 'median': median_, 'min': min_, 'max': max_, 'iqr': iqr_, 'std': std_}
     return stat_dict
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name= "pebm",
-    version="0.0.14",
+    version="1.0.2",
     use_scm_version={
         "root": '..',
         "relative_to": __file__,
