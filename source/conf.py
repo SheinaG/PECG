c9510cb405f829c5e6a4ba6bb9d70bac6f829c16
SheinaG <sheina@campus.tecnion.ac.il>
2022-11-06 09:36:23 +0200

    added code blocks to the dicumentation

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../pebm/'))
 #os.path.join('pe')
 print(sys.path)
 
@@ -87,7 +88,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 html_theme = 'sphinx_rtd_theme'
 
---
+++ source/modules.rst
@@ -0,0 +1,7 @@
+pebm
+====
+
+.. toctree::
+   :maxdepth: 4
+
+   pebm
